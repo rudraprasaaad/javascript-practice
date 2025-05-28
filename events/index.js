8337ecc4702764d31e65fbb21f268e0648302755
rudraprasaaad <goudarudra23@gmail.com>
2025-05-28 17:33:57 +0530

    .

--- events/index.js
+++ events/index.js
@@ -3,9 +3,42 @@ function handleClick() {
 }
 
 const btn = document.getElementById("myBtn2");
-
 let counter = 0;
-btn.addEventListener("click", function () {
+
+function handleCount() {
   console.log("counter", counter);
   counter++;
+}
+
+function greetMe() {
+  console.log("greeted me");
+}
+
+btn.addEventListener("click", handleCount);
+btn.addEventListener("click", greetMe);
+
+setTimeout(() => {
+  btn.removeEventListener("click", handleCount);
+}, 5 * 1000);
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM content loaded");
 });
+
+const searchElem = document.getElementById("search-id");
+
+function handleChange(e) {
+  console.log(e);
+
+  console.log("Target:", e.target);
+  console.log("Target name:", e.target.name);
+  console.log("Target value:", e.target.value);
+  console.log("event type:", e.type);
+  console.log("current target:", e.currentTarget);
+}
+
+searchElem.addEventListener("change", handleChange);
+
+function fn() {
+  console.log(this);
+}
