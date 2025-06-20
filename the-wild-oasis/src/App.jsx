8fc5385e0d251e12019ecb1a8d524fcd6b2eb05a
rudraprasaaad <goudarudra23@gmail.com>
2025-06-20 15:11:35 +0530

    added styles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -3,6 +3,23 @@ import styled from "styled-components";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
 `;
 
 function App() {
@@ -10,6 +27,9 @@ function App() {
     <>
       <div>hello world</div>
       <H1>Rudraprasad Gouda</H1>
+      <Button>Check In</Button>
+
+      <Input type="number" placeholder="Number of guests" />
     </>
   );
 }
