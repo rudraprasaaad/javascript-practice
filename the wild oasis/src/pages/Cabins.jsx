8f69607c3375afeef728e402619f2e5a143fef3a
rudraprasaaad <goudarudra23@gmail.com>
2025-06-20 19:56:31 +0530

    added  react-query

--- the wild oasis/src/pages/Cabins.jsx
+++ the wild oasis/src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   useEffect(() => {
@@ -9,15 +10,16 @@ function Cabins() {
   }, []);
 
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
+      </Row>
 
-      <img
-        src="https://ccmjulytxwdusatcjzvi.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg"
-        alt="cool"
-      />
-    </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
